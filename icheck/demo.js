c98f2b8a46d46ff55ae8b6ece30183eef9804325
imzhi <yxz_blue@126.com>
2019-09-29 18:05:58 +0800

    icheck-multi 有问题

--- icheck/demo.js
+++ icheck/demo.js
@@ -1,8 +1,80 @@
+Vue.component('icheck', {
+    template: '#vue-icheck',
+    model: {
+        prop: 'checked',
+        event: 'change',
+    },
+    props: ['checked'],
+    data: function() {
+        return {
+        };
+    },
+    mounted: function() {
+        const that = this;
+
+        $(that.$el).iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        }).on('ifUnchecked', function(event) {
+            // console.log('ifChecked', $(event.target).prop('checked'), that.checked);
+            that.checked = $(event.target).prop('checked');
+            // that.$emit('apply', event.target.value);
+        }).on('ifChecked', function(event) {
+            // console.log('ifUnchecked', $(event.target).prop('checked'), that.checked);
+            that.checked = $(event.target).prop('checked');
+            // that.$emit('apply', event.target.value);
+        });
+    },
+    watch: {
+        checked: function(val) {
+            // console.log(val);
+            $(this.$el).iCheck(val ? 'check' : 'uncheck');
+        },
+    },
+});
+
+Vue.component('icheck-multi', {
+    template: '#vue-icheck-multi',
+    model: {
+        prop: 'checked',
+        event: 'change',
+    },
+    props: ['checked'],
+    data: function() {
+        return {
+            thisval: [],
+        };
+    },
+    mounted: function() {
+        const that = this;
+
+        $(that.$el).iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        }).on('ifUnchecked', function(event) {
+            console.log('ifChecked', $(event.target).prop('checked'), that.checked);
+            // let checked = that.checked;
+            // checked.splice(checked.indexOf(event.target.value), 1);
+            // that.checked = checked;
+        }).on('ifChecked', function(event) {
+            console.log('ifUnchecked', $(event.target).prop('checked'), that.checked);
+            // that.checked.push(event.target.value);
+        });
+    },
+    watch: {
+        checked: function(val) {
+            console.log(val);
+            // const value = $(this.$el).val();
+            // $(this.$el).iCheck('' ? 'check' : 'uncheck');
+        },
+    },
+});
+
 const vue = new Vue({
     el: '#app',
     data: {
-        // selectAll: false,
-        selectNames: [2,3,4],
+        selectAll: false,
+        selectNames: [],
         list: [
             {name: '中国', val: 1},
             {name: '美国', val: 2},
@@ -11,42 +83,24 @@ const vue = new Vue({
             {name: '意大利', val: 5},
         ],
     },
-    // watch: {
-    //     selectAll: function() {
-    //         console.log(arguments);
+    methods: {
+    },
+    // computed: {
+    //     selectAll: {
+    //         get: function() {
+    //             return this.selectNames.length === this.list.length;
+    //         },
+    //         set: function(val) {
+    //             let selected = [];
+    //             if (val) {
+    //                 this.list.forEach(function(ele) {
+    //                     selected.push(ele.val);
+    //                 });
+    //                 this.selectNames = selected;
+    //             } else {
+    //                 this.selectNames = [];
+    //             }
+    //         },
     //     },
     // },
-    computed: {
-        selectAll: {
-            get: function() {
-                return this.selectNames.length === this.list.length;
-            },
-            set: function(val) {
-                let selected = [];
-                if (val) {
-                    this.list.forEach(function(ele) {
-                        selected.push(ele.val);
-                    });
-                    this.selectNames = selected;
-                } else {
-                    this.selectNames = [];
-                }
-            },
-        },
-    },
-    mounted: function() {
-        const that = this;
-        $('.icheck').iCheck({
-            checkboxClass: 'icheckbox_square-green',
-            radioClass: 'iradio_square-green',
-        // }).on('ifChecked ifUnchecked', function() {
-        //     $(this)[0].dispatchEvent(new Event('change'));
-        // });
-        }).on('ifChecked', function() {
-            that.selectNames.push($(this).val());
-        }).on('ifUnchecked', function() {
-            let selected = that.selectNames;
-            selected.splice(selected.indexOf($(this).val(), 1));
-        });
-    },
 });
